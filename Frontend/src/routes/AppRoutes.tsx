2291221b70d29487384e7e5d796e4b2398af37ae
HarshSharma-17 <harsh17sh@gmail.com>
2026-07-22 16:26:29 +0530

    add default route for vercel

--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route,  } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
@@ -12,6 +12,7 @@ import ProtectedRoute from "./ProtectedRoute";
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route
